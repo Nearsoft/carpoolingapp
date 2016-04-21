fe73e59b91510ab3296237bf94aa1366df2fa557
Victor Castrejon <vcastrejont@gmail.com>
2016-04-20 21:05:20 -0700

    fake user

--- www/js/appCtrl.js
+++ www/js/appCtrl.js
@@ -9,7 +9,20 @@ function appCtrl($scope, authFactory, $state, profileFactory,
   $scope.login = login;
   $scope.logout = logout;
 
-  userInit();
+  function userFakeInit() {
+     $scope.currentUser = {
+       "id" : "56d8b59351cf7c9c0b720b79",
+       "provider_id" : "117613786321394401804",
+       "provider" : "google",
+       "name" : "Rafa Manrique",
+       "photo" : "https://lh3.googleusercontent.com/-XdUIqdMkCWA/AAAAAAAAAAI/AAAAAAAAAAA/4252rscbv5M/photo.jpg?sz=50",
+       "email" : "rmanrique@nearsoft.com"
+     };
+     $scope.isAuthenticated = true;
+   }
+
+   //userInit();
+   userFakeInit();
 
   function userInit() {
     $scope.currentUser = null;
